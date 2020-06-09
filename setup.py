3e0dfe1e84ebab20f569588a4069ba2a65ea4cb9
BuildTools <unconfigured@null.spigotmc.org>
2020-06-09 16:32:22 -0500

    Minor changes to setup.py

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ with open("README.txt", "r") as fn:
     long_description = fn.read()
 
 setuptools.setup(
-    name="pysonde", # Replace with your own username
+    name="sodoesaburningbus", # Replace with your own username
     version="0.0.1",
-    author="sodoesaburningbus",
+    author="Christopher Phillips of UAH, Huntsville, Alabama",
     author_email="cephillips574@gmail.com",
     description="A python package for reading and analyzing several common weather balloon sounding formats.",
     long_description=long_description,
-    long_description_content_type="text",
-    url="https://github.com/sodoesaburningbus/pysonde/",
+    long_description_content_type="text/markdown",
+    url="https://github.com/sodoesaburningbus/pysonde",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
